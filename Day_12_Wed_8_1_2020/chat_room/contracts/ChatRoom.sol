9f410ae41ecd2dc3ece8b77177ae554ff640de2c
I.A <ilyas.alfan.b@gmail.com>
2020-01-12 22:23:26 +0100

    Add NatSpec comments

--- Day_12_Wed_8_1_2020/chat_room/contracts/ChatRoom.sol
+++ Day_12_Wed_8_1_2020/chat_room/contracts/ChatRoom.sol
@@ -1,55 +1,38 @@
 pragma solidity >=0.4.25 <0.6.0;
 
-/**
- * A smart contract that simulates a chat room where
- * participants can chat with each other.
- */
+/// @title A chat room for users to participate in.
+/// @author I.A Baas
+/// @notice You can use this contract for only the most basic chatting.
 contract ChatRoom {
-    /**
-     * Defines an event of a participant having joined the chat room.
-     */
+    /// @notice Defines an event of a participant having joined the chat room.
     event ParticipantJoined(string displayName);
 
-    /**
-     * Defines a message broadcast event that subscribers may listen
-     * to, to trigger an action in a separate contract.
-     */
+    /// @notice Defines a message broadcast event that subscribers may listen
+    /// to, to trigger an action in a separate contract.
     event MessageBroadcasted(string displayName, string text);
 
-    /**
-     * Defines an event of a participant having left the chat room.
-     */
+    /// @notice Defines an event of a participant having left the chat room.
     event ParticipantLeft(string displayName);
 
-    /**
-     * Defines an event of the chat room having been opened again.
-     */
+    /// @notice Defines an event of the chat room having been opened again.
     event ChatRoomOpened();
 
-    /**
-     * Defines an event of the chat room having been closed again.
-     */
+    /// @notice Defines an event of the chat room having been closed again.
     event ChatRoomClosed();
 
-    /**
-     * A published chat message.
-     */
+    /// @notice A published chat message.
     struct Message {
         string text;
     }
 
-    /**
-     * A participant.
-     */
+    /// @notice A participant.
     struct Participant {
         uint index;
         string displayName;
         bool deleted;
     }
 
-    /**
-     * The current state of the chat room.
-     */
+    /// @notice The state of the chat room.
     enum State {
         OPEN, CLOSED
     }
@@ -58,9 +41,9 @@ contract ChatRoom {
     Message[] private history;
     State private state = State.OPEN;
 
-    /**
-     * Registers a new 'Participant' to this chat room.
-     */
+    /// @notice Registers a new 'Participant' to this chat room.
+    /// @param displayName The name of the participant to display to
+    /// other users.
     function register(string memory displayName) public {
         // in every other language you'd do something like : participants.push(Participant(displayName, new Participant[]));
         // but solidity is an incredibly odd language so we'd have to instantiate it this way:
@@ -76,32 +59,27 @@ contract ChatRoom {
         emit ParticipantJoined(displayName);
     }
 
-    /**
-     * Opens up the chat room.
-     */
+    /// @notice Opens up the chat room so that users may chatting.
     function open() public {
         state = State.OPEN;
         emit ChatRoomOpened();
     }
 
-    /**
-     * Closes off the chat room.
-     */
+    /// @notice Closes off the chat room so that users can no longer chat.
     function close() public {
         state = State.CLOSED;
         emit ChatRoomClosed();
     }
 
-    /**
-     * Returns whether this chat room is open for participants or not.
-     */
+    /// @notice Returns whether this chat room is open for participants or not.
+    /// @return Whether the chat room is open or not.
     function isOpen() public view returns(bool isOpened) {
         return state == State.OPEN;
     }
 
-    /**
-     * Broadcasts the given text message as being sent by the specified user.
-     */
+    /// @notice Broadcasts the given text message as being sent by the specified user.
+    /// @param displayName The name of the participant sending the message.
+    /// @param text The text to broadcast.
     function broadcast(string memory displayName, string memory text) public {
         require(state != State.CLOSED, "Chat room is closed");
         require(bytes(text).length != 0, "Cannot publish empty text values");
@@ -110,9 +88,8 @@ contract ChatRoom {
         emit MessageBroadcasted(displayName, text);
     }
 
-    /**
-     * Unregisters a 'Participant' from this chat room.
-     */
+    /// @notice Unregisters a 'Participant' from this chat room.
+    /// @param displayName The name put up for display of a participant.
     function unregister(string memory displayName) public {
         participantsByNames[displayName].deleted = true;
 
@@ -120,18 +97,16 @@ contract ChatRoom {
         emit ParticipantLeft(displayName);
     }
 
-    /**
-     * Returns whether a user by the specified display name exists
-     * within this chat room.
-     */
+    /// @notice Returns whether a user by the specified display name exists within
+    /// this chat room.
+    /// @return Whether the specified user exists in this chat room.
     function isRegistered(string memory displayName) public view returns(bool exists) {
         return !participantsByNames[displayName].deleted;
     }
 
-    /**
-     * Returns the amount of messages that have been published in this
-     * chat room.
-     */
+    /// @notice Returns the amount of messages that have been published
+    /// in this chat room.
+    /// @return The amount of messages that have been published.
     function getMessageHistorySize() public view returns(uint count) {
         return history.length;
     }
