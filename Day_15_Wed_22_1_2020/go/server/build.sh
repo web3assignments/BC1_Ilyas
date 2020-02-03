e32e4e9df4ad5037b85f0e6a5acfcf6c966e1ddd
I.A <ilyas.alfan.b@gmail.com>
2020-02-04 00:06:10 +0100

    Update build.sh

--- Day_15_Wed_22_1_2020/go/server/build.sh
+++ Day_15_Wed_22_1_2020/go/server/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-solc --abi ../solidity/contracts/ChatRoom.sol -o abi --overwrite && solc --bin ../solidity/contracts/ChatRoom.sol -o abi --overwrite
+solc --abi ../../solidity/contracts/ChatRoom.sol -o abi --overwrite && solc --bin ../../solidity/contracts/ChatRoom.sol -o abi --overwrite
 
-abigen --bin=../go/abi/ChatRoom.bin --sol ../solidity/contracts/ChatRoom.sol --pkg contracts --out contracts/chatroom.go
+abigen --bin=../../go/abi/ChatRoom.bin --sol ../../solidity/contracts/ChatRoom.sol --pkg contracts --out contracts/chatroom.go
